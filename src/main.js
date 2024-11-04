83571b6f1bbcb76ea104fa0e55da1e8c3cbc7a34
Emilio Gallo <emilio.gallo17@gmail.com>
2024-11-04 18:36:47 +0100

    feat: project scaffolding and dependencies installation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import './scss/general.scss';
+
+import "bootstrap";
+
+import App from './App.vue';
+
+createApp(App).mount('#app');
