b7fde44c07cee9b87cd1c4415a75196ec4e1b101
Emilio Gallo <emilio.gallo17@gmail.com>
2024-11-06 21:21:13 +0100

    feat: added two-way binding to input number and select - components/CurrencyInput.vue

--- src/components/CurrencyInput.vue
+++ src/components/CurrencyInput.vue
@@ -13,7 +13,25 @@ export default {
    data() {
       return {
 
+         amount: this.inputAmount,
+         selectedCurrencyValue: this.selectedCurrency,
       };
+   },
+
+   methods: {
+
+      updateAmount() {
+
+         this.$emit('input-change', this.amount);
+
+      },
+
+      updateCurrency() {
+
+         this.$emit('currency-change', this.selectedCurrencyValue);
+
+      },
+
    },
 
 }
@@ -23,11 +41,13 @@ export default {
 
    <div class="input-group col-2">
 
-      <input class="btn btn-light" type="number" :value="inputAmount">
+      <input class="btn btn-light" type="number" v-model="amount" @input="updateAmount">
 
-      <select class="form-select btn btn-outline-light">
+      <select class="form-select btn btn-outline-light" v-model="selectedCurrencyValue" @change="updateCurrency">
 
-         <option v-for="(currency, i) in currenciesKeys" :key="currency" :value="currency" :selected="selectedCurrency == currency">{{ `${currenciesValues[i]} (${currency})` }}</option>
+         <option v-for="(currency, i) in currenciesKeys" :key="currency" :value="currency">
+            {{ `${currenciesValues[i]} (${currency})` }}
+         </option>
 
       </select>
 
