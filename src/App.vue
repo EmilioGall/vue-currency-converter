7531a6d5e0ea3a1ec76d8639b0d79c96978ac430
Emilio Gallo <emilio.gallo17@gmail.com>
2024-11-08 12:34:21 +0100

    refactory: commented and organized - App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import CurrencyChart from './components/CurrencyChart.vue';
 
 export default {
 
+  // Register imported components for use in the template
   components: {
 
     CurrencyInput,
@@ -15,96 +16,113 @@ export default {
 
   },
 
+  // Define the data properties for this component
   data() {
     return {
 
-      currenciesKeys: [],
-      currenciesValues: [],
-      amount1: 0,
-      amount2: 0,
-      selectedCurrency1: 'EUR',
-      selectedCurrency2: 'USD',
-      conversionRate: 0,
+      currenciesKeys: [], // Arrays to hold currency codes
+      currenciesValues: [], // Arrays to hold currency names
+      amount1: 0, // First currency amount
+      amount2: 0, // Second currency amount
+      selectedCurrency1: 'EUR', // Default selected first currencies
+      selectedCurrency2: 'USD', // Default selected second currencies
+      conversionRate: 0, // Default conversion rate from selectedCurrency1 to selectedCurrency2
 
     };
   },
 
+  // Lifecycle hook that runs when the component is created
   async created() {
 
+    // Fetch the list of available currencies from the API
     await this.fetchCurrencies();
 
+    // Fetch the conversion rate for the default selected currencies
     await this.fetchConvertionRate(this.selectedCurrency1, this.selectedCurrency2);
 
   },
 
+  // Methods for the component's functionality
   methods: {
 
+    // Method to fetch available currencies from the API
     async fetchCurrencies() {
 
+      // Send a GET request to the currency API
       const response = await axios.get('https://api.frankfurter.app/currencies');
 
       // console.log('response', response.data);
 
+      // Store the currency codes in [currenciesKeys] and currency names in [currenciesValues]
       this.currenciesKeys = Object.keys(response.data);
       this.currenciesValues = Object.values(response.data);
 
     },
 
+    // Method to fetch the conversion rate between two currencies
     async fetchConvertionRate(curFrom, curTo) {
 
+      // Send a GET request to get the conversion rate for the selected currencies
       const response = await axios.get(`https://api.frankfurter.app/latest?base=${curFrom}&symbols=${curTo}`);
 
       // console.log('response', response);
 
+      // Store the conversion rate in the component's data [conversionRate]
       this.conversionRate = response.data.rates[curTo];
 
       console.log('conversionRate =', this.conversionRate, typeof this.conversionRate);
 
-      this.amount2 = this.amount1 * this.conversionRate;
-
     },
 
+    // Method to update [amount1] and corresponding [amount2] when the first input changes
     updateAmount1(amount) {
 
-      this.amount1 = amount;
+      // Update the [amount1] with the new value
+      this.amount1 = Number(amount);
 
-      console.log('amount1 =', this.amount1, typeof this.amount1);
-
-      console.log('conversionRate =', this.conversionRate, typeof this.conversionRate);
-
-      this.amount2 = (this.amount1 * this.conversionRate).toFixed(2);
-
-      console.log('amount2 =', this.amount2, typeof this.amount2);
+      // Calculate and update [amount2] based on the current conversion rate
+      this.amount2 = Number((this.amount1 * this.conversionRate).toFixed(2));
 
     },
 
+    // Method to update [amount2] and corresponding [amount1] when the second input changes
     updateAmount2(amount) {
 
-      this.amount2 = amount;
+      // Update the [amount2] with the new value
+      this.amount2 = Number(amount);
 
-      this.amount1 = (this.amount2 / this.conversionRate).toFixed(2);
-
-      console.log('amount2 =', this.amount2);
+      // Calculate and update [amount1] based on the current conversion rate
+      this.amount1 = Number((this.amount2 / this.conversionRate).toFixed(2));
 
     },
 
-    updateCurrency1(currency) {
+    // Method to update [selectedCurrency1] and fetch the new conversion rate
+    async updateCurrency1(currency) {
 
+      // Update [selectedCurrency1] with the newly selected currency
       this.selectedCurrency1 = currency;
 
-      console.log('selectedCurrency1 =', this.selectedCurrency1);
+      // Fetch the conversion rate for the new currency selection
+      await this.fetchConvertionRate(this.selectedCurrency1, this.selectedCurrency2);
 
-      this.fetchConvertionRate(this.selectedCurrency1, this.selectedCurrency2);
+      // Calculate and update [amount1] based on the current conversion rate
+      this.updateAmount1(this.amount1);
 
     },
 
-    updateCurrency2(currency) {
+    // Method to update [selectedCurrency2] and fetch the new conversion rate
+    async updateCurrency2(currency) {
 
+      // Update [selectedCurrency1] with the newly selected currency
       this.selectedCurrency2 = currency;
 
       console.log('selectedCurrency2 =', this.selectedCurrency2);
 
-      this.fetchConvertionRate(this.selectedCurrency1, this.selectedCurrency2);
+      // Fetch the conversion rate for the new currency selection
+      await this.fetchConvertionRate(this.selectedCurrency1, this.selectedCurrency2);
+
+      // Calculate and update [amount2] based on the current conversion rate
+      this.updateAmount2(this.amount2);
 
     },
 
@@ -126,22 +144,22 @@ export default {
 
       <span class="fs-2 text-white">{{ conversionRate + ' ' + selectedCurrency2 }}</span>
 
-      <!-- First Input Group -->
+      <!-- First Input Group for the first currency -->
       <CurrencyInput class="col" :currenciesKeys="currenciesKeys" :currenciesValues="currenciesValues"
         :selectedCurrency="selectedCurrency1" v-model:inputAmount="amount1" @input-change="updateAmount1"
         @currency-change="updateCurrency1" :disabledCurrencies="[selectedCurrency2]" />
       <!-- /First Input Group -->
 
-      <!-- Second Input Group -->
+      <!-- Second Input Group for the second currency -->
       <CurrencyInput class="col" :currenciesKeys="currenciesKeys" :currenciesValues="currenciesValues"
         :selectedCurrency="selectedCurrency2" v-model:inputAmount="amount2" @input-change="updateAmount2"
         @currency-change="updateCurrency2" :disabledCurrencies="[selectedCurrency1]" />
       <!-- /Second Input Group -->
 
       <div class="col border rounded-2">
-  
+
         <CurrencyChart :selectedCurrency1="selectedCurrency1" :selectedCurrency2="selectedCurrency2"/>
-  
+
       </div>
 
     </div>
